32eb44855777abbf9fda5bff32d29b4aae0c9028
Christopher Lee-Marshall <161063506+LeeMarshallChris@users.noreply.github.com>
2024-05-03 13:56:47 -0500

    Add files via upload

---
+++ Gradebook Calculator/SchoolCourse+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  SchoolCourse+CoreDataClass.swift
+//  Gradebook Calculator
+//
+//  Created by  on 5/3/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(SchoolCourse)
+public class SchoolCourse: NSManagedObject {
+
+}
